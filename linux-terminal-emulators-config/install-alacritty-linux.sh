7962ae3ec2ca577e7b778ed3caff335adde0929d
David Craddock <>
2025-05-14 08:19:48 +0100

    Sort out linux alacritty

--- linux-terminal-emulators-config/install-alacritty-linux.sh
+++ linux-terminal-emulators-config/install-alacritty-linux.sh
@@ -4,9 +4,11 @@ set -x
 set -e
 
 sudo snap install --classic alacritty || sudo snap update --classic alacritty
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /bin/alacritty 50
 mkdir -p $HOME/.config/alacritty/
 cp ~/.dotfiles/linux-terminal-emulators-config/linux-alacritty.toml $HOME/.config/alacritty/alacritty.toml
 ~/.dotfiles/linux-terminal-emulators-config/install-nerdfont-linux.sh
+
 echo "Now start alacritty from the search applications menu, and then pin it to the dock"
 
 
