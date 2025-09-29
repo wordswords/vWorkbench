6dc9483fcecb0fb1c7b141c0373d12d831f4ebf9
David Craddock <contact@davidcraddock.net>
2025-09-29 15:43:50 +0100

    Fixed

--- bin/grep-for-urls-in-file.sh
+++ bin/grep-for-urls-in-file.sh
@@ -4,17 +4,18 @@ set -x
 set -e
 
 filename=$1
-if [[ -z $filename ]]
-then
- echo "Invalid arguments"
- printHelp
-fi
 
 printHelp()
 {
  echo "Usage: $0 <file to grep urls from>"
  exit 1
 }
+
+if [[ -z $filename ]]
+then
+ echo "Invalid arguments"
+ printHelp
+fi
 
 cat ${filename} | grep -oE '\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]'
 
