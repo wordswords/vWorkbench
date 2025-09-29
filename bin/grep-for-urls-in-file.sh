5e57110d071921e21495a8011e529679e64cb583
David Craddock <contact@davidcraddock.net>
2025-09-29 15:42:48 +0100

    Useful script..

---
+++ bin/grep-for-urls-in-file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+set -e
+
+filename=$1
+if [[ -z $filename ]]
+then
+ echo "Invalid arguments"
+ printHelp
+fi
+
+printHelp()
+{
+ echo "Usage: $0 <file to grep urls from>"
+ exit 1
+}
+
+cat ${filename} | grep -oE '\b(https?|ftp|file)://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]'
+
