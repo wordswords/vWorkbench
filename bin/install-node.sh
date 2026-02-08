45f5dc1f24c64c243e1280a44b470404098cc3b8
David Craddock <contact@davidcraddock.net>
2026-02-08 09:33:01 +0000

    This is needed in latest debian

--- bin/install-node.sh
+++ bin/install-node.sh
@@ -18,4 +18,4 @@ sudo cat /etc/apt/sources.list.d/nodesource.list | sort -u | sudo tee /etc/apt/s
 sudo mv /etc/apt/sources.list.d/nodesource.list.dedupe /etc/apt/sources.list.d/nodesource.list
 
 sudo apt-get update
-sudo apt-get install nodejs -y
+sudo apt-get install nodejs node-corepack -y
