02cec3f6f8aadc69e17e3053af5fae1a11f48c05
David Craddock <contact@davidcraddock.net>
2026-07-30 18:06:37 +0100

    Fix firefox script

--- bin/install-firefox-linux.sh
+++ bin/install-firefox-linux.sh
@@ -2,8 +2,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Install Firefox on AlmaLinux via RPM Fusion
-sudo dnf install -y epel-release
-sudo dnf install -y --nogpgcheck https://download1.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm
 sudo dnf install -y firefox
 
