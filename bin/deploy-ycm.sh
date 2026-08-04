3172ef44503ea9060f5180c72ff3b1133e479935
David Craddock <contact@davidcraddock.net>
2026-08-04 12:06:10 +0100

    Remove this as causing problems!

--- bin/deploy-ycm.sh
+++ bin/deploy-ycm.sh
@@ -2,10 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Compile and install python 3.12
-~/.dotfiles/bin/compile-python-almalinux.sh
-export alias python3="python3.12"
-
 # Assumes node is already installed - Installs all other prereqs for YCM
 sudo dnf install -y mono-complete golang
 sudo dnf install -y java-latest-openjdk java-latest-openjdk-devel
