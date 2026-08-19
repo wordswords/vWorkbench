029e344b0c3cca8d966b4e73ffab5aa6dd14969d
David Craddock <contact@davidcraddock.net>
2026-08-19 16:08:08 +0100

    Decrpt ps3 iso image using ird

---
+++ bin/decrypt-ps3-iso-image-using-ird.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+set -e
+
+python -m libray.libray -i /home/games/PS3/Ketsui\ -\ Kizuna\ Jigoku-tachi\ Extra\ \(Japan\).iso -k /home/david/ketsui.ird -o /home/games/PS3/Ketsui\ -\ Kizuna\ Jigoku-tachi\ Extra\ \(Japan\)_decrypted.iso
+
