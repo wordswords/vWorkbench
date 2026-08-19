b8a35a04b2052f448986e3228a9cd3e334ca4ac6
David Craddock <contact@davidcraddock.net>
2026-08-19 20:47:25 +0100

    Version that actually installs elixir

--- bin/install-elixir-almalinux.sh
+++ bin/install-elixir-almalinux.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-ELIXIR_VERSION="1.19.5"
+set -x
+set -e
+
+ELIXIR_VERSION="1.20-latest"
 OTP_MAJOR="$(erl -noshell -eval 'io:format("~s~n", [erlang:system_info(otp_release)]), halt().')"
 
 sudo mkdir -p /opt/elixir
 cd /tmp
 
+echo "Trying to download https://github.com/elixir-lang/elixir/releases/download/v${ELIXIR_VERSION}/elixir-otp-${OTP_MAJOR}.zip"
 curl -fL -o elixir.zip \
   "https://github.com/elixir-lang/elixir/releases/download/v${ELIXIR_VERSION}/elixir-otp-${OTP_MAJOR}.zip"
 
