5594fd87a482005b3d546d1ec1e9acdaf4cc9276
David Craddock <contact@davidcraddock.net>
2026-08-20 20:10:46 +0100

    Refactor scripts for Clean Code and fix OCR/clipboard bugs

--- bin/install-adom.sh
+++ bin/install-adom.sh
@@ -6,10 +6,14 @@ IFS=$'\n\t'
 
 # ADOM_VERSION can be set via the environment (exported by deploy.sh).
 ADOM_VERSION="${ADOM_VERSION:-3.3.3}"
+ARCHIVE="adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
 
 sudo dnf install -y ncurses-libs
-wget "https://www.adom.de/home/download/current/adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
-tar xzf "adom_linux_ubuntu_64_${ADOM_VERSION}.tar.gz"
-cp ./adom*/adom ~/bin
-rm -rf ./adom*
+wget "https://www.adom.de/home/download/current/${ARCHIVE}"
+tar xzf "${ARCHIVE}"
+
+# The archive extracts into a versioned adom*/ directory.
+sudo install -m 755 ./adom*/adom "${HOME}/bin/adom"
+find . -maxdepth 1 -type d -name 'adom*' -exec rm -rf {} +
+rm -f "${ARCHIVE}"
 
